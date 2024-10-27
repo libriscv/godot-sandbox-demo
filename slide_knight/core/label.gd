59c712af74793c91f2dcbfec8a580e92990d1c1f
Alf-André Walla <fwsgonzo@hotmail.com>
2024-10-27 10:46:20 +0100

    Update to v0.27

--- slide_knight/core/label.gd
+++ slide_knight/core/label.gd
@@ -1,12 +1,16 @@
 extends Label
 
-@export var player_elf : Sandbox_SlideKnightCorePlayerPlayer
+var player_sandbox : Sandbox
 
 func _ready() -> void:
-	self.text = player_elf.get("player_name")
+	var player = get_node("%Player")
+	# Accessing a shared sandbox requires using get_sandbox_for(), which takes a Node instance
+	player_sandbox = player.get_script().get_sandbox_for(player)
+	# Now we can access the Sandbox used by Player
+	self.text = player_sandbox.get("player_name")
 
 func _process(_delta: float) -> void:
 	if self.name == "CallLabel":
 		# Get any sandbox, and ask for the global number of VM calls made
-		var total_calls = player_elf.get("global_calls_made")
+		var total_calls = player_sandbox.get("global_calls_made")
 		self.text = "Virtual Machine\nfunction calls: " + str(total_calls)
--- slide_knight/core/slime/slime.cpp
+++ slide_knight/core/slime/slime.cpp
@@ -17,25 +17,28 @@ PER_OBJECT(SlimeState);
 
 extern "C" Variant _physics_process(double delta) {
 	if (is_editor()) {
-		Node("AnimatedSprite2D")("play", "idle");
+		AnimatedSprite2D sprite("AnimatedSprite2D");
+		sprite.play("idle");
+		Time t = Time::get_singleton();
+		sprite.set_flip_h((t.get_ticks_msec() / 2560) % 2 == 0);
 		return Nil;
 	}
 
 	Node2D slime = get_node();
-	Node2D sprite("AnimatedSprite2D");
+	AnimatedSprite2D sprite("AnimatedSprite2D");
 	auto& state = GetSlimeState(slime);
 
 	Vector2 spd = Vector2(slime_speed, 0.0f) * float(delta) * state.direction;
 	slime.set_position(slime.get_position() + spd);
 	// Change direction if rays collide
-	if (state.direction > 0 && Node("raycast_right")("is_colliding")) {
+	if (state.direction > 0 && RayCast2D("raycast_right").is_colliding()) {
 		state.direction = -1;
-		sprite.set("flip_h", true);
-		sprite("play", std::u32string(U"spawn"));
-	} else if (state.direction < 0 && Node("raycast_left")("is_colliding")) {
+		sprite.set_flip_h(true);
+		sprite.play("spawn");
+	} else if (state.direction < 0 && RayCast2D("raycast_left").is_colliding()) {
 		state.direction = 1;
-		sprite.set("flip_h", false);
-		sprite("play", "idle");
+		sprite.set_flip_h(false);
+		sprite.play("idle");
 	}
 	return Nil;
 }
