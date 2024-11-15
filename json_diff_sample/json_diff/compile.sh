d36a08aec347c6fedf8d9f35269f445e457a7f12
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2024-11-15 13:31:36 -0800

    Remove ccache.

--- json_diff_sample/json_diff/compile.sh
+++ json_diff_sample/json_diff/compile.sh
@@ -5,6 +5,6 @@
 # Create build directory
 mkdir -p build
 pushd build
-AR="riscv64-unknown-elf-ar" CXX="ccache riscv64-unknown-elf-g++" CC="ccache riscv64-unknown-elf-gcc" cmake .. -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=../cmake/toolchain.cmake
+AR="riscv64-unknown-elf-ar" CXX="riscv64-unknown-elf-g++" CC="riscv64-unknown-elf-gcc" cmake .. -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=../cmake/toolchain.cmake
 cmake --build .
 popd
